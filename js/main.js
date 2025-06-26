7e8c492b1fb9f4459b3f02a2a489101fe102d492
001 <152649123+theredplanetsings@users.noreply.github.com>
2025-06-26 10:03:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,7 +70,6 @@ const observerOptions = {
     threshold: 0.1,
     rootMargin: '0px 0px -50px 0px'
 };
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
